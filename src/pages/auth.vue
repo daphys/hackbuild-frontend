eb39efc00eed6f57908507f57cff40d2e2545378
Vlad <103288921+mbdaphys@users.noreply.github.com>
2022-07-16 00:54:38 +0300

    master commit

---
+++ src/pages/auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="auth">
+      <a-input>
+        <template #prefix> <img src="/icons/icon_auth.svg" alt="" /> </template
+      ></a-input>
+      <a-input></a-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style  scoped>
+.auth{
+    max-width: 300px;
+}
+</style>
